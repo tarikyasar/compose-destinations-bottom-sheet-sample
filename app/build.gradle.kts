2986a5dc5dc164f1db3ad945af119ecd14b23c92
Tarik Yasar <m.tarikyasar@gmail.com>
2024-01-17 20:56:13 +0300

    Add compose-destinations dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp") version "1.9.0-1.0.13"
 }
 
 android {
@@ -66,4 +67,8 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // Compose Destinations
+    implementation("io.github.raamcosta.compose-destinations:core:1.9.60")
+    ksp("io.github.raamcosta.compose-destinations:ksp:1.9.60")
 }
